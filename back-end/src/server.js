c6b35ce40a2d0e02a98b45b202089535c862f516
Sean Bunch <100799798+SeanBunch@users.noreply.github.com>
2023-05-26 16:08:59 -0400

    update to server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -19,7 +19,7 @@ knex.migrate
     console.log("migrations", migrations);
     app.use(express.static(path.join(__dirname, "build")));
 
-    app.get("/", function (req, res) {
+    app.get("/*", function (req, res) {
       res.sendFile(path.join(__dirname, "build", "index.html"));
     });
     app.listen(PORT, listener);
